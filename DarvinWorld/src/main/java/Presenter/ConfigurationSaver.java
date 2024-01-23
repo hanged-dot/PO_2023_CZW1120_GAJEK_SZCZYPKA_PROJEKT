7044363a98ebb2f64cf54479b6dbbe5c4662612a
DESKTOP-M5P6EVP\Pysiokotka <maja.slowik@gmail.com>
2024-01-23 21:10:24 +0100

    fixed paths

--- DarvinWorld/src/main/java/Presenter/ConfigurationSaver.java
+++ DarvinWorld/src/main/java/Presenter/ConfigurationSaver.java
@@ -8,18 +8,20 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class ConfigurationSaver {
 
     public static void saveConfiguration(String name, SimulationProperties simulationProperties){
-
         try{
-            FileOutputStream fileOutputStream = new FileOutputStream(new File("src/main/resources/configurations/"+name+".txt"));
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("PO_2023_CZW1120_GAJEK_SZCZYPKA_PROJEKT/DarvinWorld/src/main/resources/configurations/"+name+".txt"));
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 
             objectOutputStream.writeObject(simulationProperties);
             objectOutputStream.flush();
             objectOutputStream.close();
+            fileOutputStream.flush();
             fileOutputStream.close();
 
         } catch (IOException e){
